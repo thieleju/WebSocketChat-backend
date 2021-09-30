c73ff03401714ddf1120e82d042f802eb0c6f523
Julian <thiele.juli@gmail.com>
2021-09-30 21:09:51 +0200

    Implemented basic websocket communication

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 const express = require("express");
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
 
-const port = 3000;
+const port = 4200;
+const origin = "http://localhost:8080";
 
+var stats = {
+  connections: [],
+};
 
-io.on('connection', (socket) => {
-    
+const server = app.listen(port, () => {
+  console.log("listening for requests on port " + port);
 });
 
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+const io = require("socket.io")(server, {
+  cors: {
+    origin,
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
 });
-  
+
+io.on("connection", (socket) => {
+  stats.connections.push({ id: socket.id });
+  console.log(
+    stats.connections.length,
+    "✅",
+    socket.id,
+    socket.handshake.address
+  );
+
+  socket.emit("greetings", "Hello from the backend!");
+
+  socket.on("chat_message", (message) => {
+    io.emit("chat_message", socket.id, message)
+  })
+
+  socket.on("disconnect", () => {
+    stats.connections = stats.connections.filter((el) => el.id !== socket.id);
+    console.log(
+      stats.connections.length,
+      "❌",
+      socket.id,
+      socket.handshake.address
+    );
+  });
+});
+
