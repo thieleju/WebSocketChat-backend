001bb1280c514492326bc660c159e118d79c380e
Julian <thiele.juli@gmail.com>
2021-10-06 22:49:25 +0200

    Added multiple channels

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const app = express();
 
 const port = 3000;
 const origin = "http://localhost:8080";
+const maxMessagesPerChannel = 100;
 
 var stats = {
   connections: [],
-  messages: []
+  messages: [],
 };
 
 const server = app.listen(port, () => {
@@ -21,6 +22,8 @@ const io = require("socket.io")(server, {
   },
 });
 
+var chat = require("./chatSettings.json");
+
 io.on("connection", (socket) => {
   stats.connections.push({ id: socket.id });
   console.log(
@@ -30,16 +33,73 @@ io.on("connection", (socket) => {
     socket.handshake.address
   );
 
+  socket.emit("greetings", "Hello from the server!");
 
-  socket.emit("greetings", "Hello from the backend!");
-  socket.emit("current_connected_count", stats.connections.length)
-  socket.emit("chat_old_messages", stats.messages)
+  var details = {
+    menu_name: chat.menu_name,
+    channels: [],
+  };
+  chat.channels.forEach((el) => {
+    details.channels.push({
+      id: el.id,
+      title: el.title,
+      icon: el.icon,
+      channelID: el.channelID,
+    });
+  });
+  socket.emit("channel_details", details);
 
-  // send message to everyone
-  socket.on("chat_message", (message) => {
-    stats.messages.push({id: socket.id, msg:replaceLineBreaksWithBr(message)})
-    io.emit("chat_message", socket.id, replaceLineBreaksWithBr(message))
-  })
+  socket.on("add_message_to_channel", (data) => {
+    try {
+      var messageObj = {
+        socketID: socket.id,
+        socketAddress: socket.handshake.address,
+        text: replaceLineBreaksWithBr(data.message),
+        user: {
+          name: data.user.name,
+          color: data.user.color,
+        },
+      };
+      // check if messages are at maximum
+      var channel = chat.channels.find((el) => data.channelID == el.channelID)
+      if (channel.messages.length >= maxMessagesPerChannel) {
+        // remove first message to add another at the end
+        channel.messages.shift()
+        channel.messages.push(messageObj)
+      } else {
+        channel.messages.push(messageObj)
+      }
+      // apply changes to array
+      chat.channels
+        .find((el) => data.channelID == el.channelID).messages = channel.messages
+
+      var updateObj = {
+        channelID: data.channelID,
+        channel: chat.channels.find((el) => data.channelID == el.channelID),
+      };
+      io.emit("messages_update_for_channel", updateObj);
+    } catch (error) {
+      console.log(error);
+      io.emit("error", "Something went wrong! " + error);
+    }
+  });
+
+  socket.on("update_messages_for_channel", (channelID) => {
+    try {
+      var channel = chat.channels.find((el) => channelID == el.channelID);
+      // check if channel exists
+      if (channel) {
+        var updateObj = {
+          channelID,
+          channel,
+        };
+        io.emit("messages_update_for_channel", updateObj);
+      }
+    } catch (error) {
+      console.log(error);
+      io.emit("error", error);
+    }
+  });
 
   socket.on("disconnect", () => {
     stats.connections = stats.connections.filter((el) => el.id !== socket.id);
