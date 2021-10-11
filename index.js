22a56de2ddbecae5f237ac8d94956390a634ac4a
Julian <thiele.juli@gmail.com>
2021-10-11 21:36:22 +0200

    Added dsgvo, impressum

--- index.js
+++ index.js
@@ -1,19 +1,64 @@
 const express = require("express");
 const app = express();
-const helmet = require('helmet');
+const fs = require("fs");
+const helmet = require("helmet");
 // set dynamic env filename
-require('dotenv').config({ path: `./.env.${process.env.NODE_ENV}` })
+require("dotenv").config({ path: `./.env.${process.env.NODE_ENV}` });
 
-app.use(helmet())
+app.use(helmet());
 
 const port = process.env.APP_PORT;
 const origin = process.env.APP_ORIGIN;
-const maxMessagesPerChannel = process.env.APP_MAX_MESSAGES
+const maxMessagesPerChannel = process.env.APP_MAX_MESSAGES;
 
 var stats = {
   connections: [],
   messages: [],
 };
+
+app.use(require("cors")());
+
+app
+  // middleware able to get requestors ip by req.ip
+  .set("trust proxy", true)
+  // use cors
+  .use(require("cors")())
+  .use(express.json())
+  .use(
+    express.urlencoded({
+      extended: true,
+    })
+  )
+  // check if provided data is a valid json to catch unhandled errors
+  .use((err, req, res, next) => {
+    if (err) {
+      res.status(400).json({ status: "error", message: "Invalid request!" });
+    }
+  });
+
+app.get("/impressum", (req, res) => {
+  try {
+    var impressum = fs.readFileSync("assets/impressum.html");
+    res.send(impressum);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+app.get("/dsgvo", (req, res) => {
+  try {
+    var dsgvo = fs.readFileSync("assets/dsgvo.html");
+    res.send(dsgvo);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+app.use("/", (req, res) => {
+  res
+    .status(200)
+    .json({ status: "success", message: "This is the chat.node5.de api" });
+});
 
 const server = app.listen(port, () => {
   console.log("Server listening for requests on port " + port);
@@ -39,7 +84,7 @@ io.on("connection", (socket) => {
 
   // add user object
   stats.connections.push({ id: socket.id, username: "", color: "" });
-  
+
   // emit channel details for client on Load
   var details = {
     menu_name: chat.menu_name,
@@ -58,7 +103,7 @@ io.on("connection", (socket) => {
   // listen for message send event
   socket.on("add_message_to_channel", (data) => {
     try {
-      var findUser = stats.connections.find((el) => el.id == socket.id)
+      var findUser = stats.connections.find((el) => el.id == socket.id);
       var messageObj = {
         socketID: socket.id,
         socketAddress: socket.handshake.address,
@@ -68,19 +113,19 @@ io.on("connection", (socket) => {
           color: findUser.color,
         },
       };
-      
+
       // check if messages are at maximum
-      var channel = chat.channels.find((el) => data.channelID == el.channelID)
+      var channel = chat.channels.find((el) => data.channelID == el.channelID);
       if (channel.messages.length >= maxMessagesPerChannel) {
         // remove first message to add another at the end
-        channel.messages.shift()
-        channel.messages.push(messageObj)
+        channel.messages.shift();
+        channel.messages.push(messageObj);
       } else {
-        channel.messages.push(messageObj)
+        channel.messages.push(messageObj);
       }
       // apply changes to array
-      chat.channels
-        .find((el) => data.channelID == el.channelID).messages = channel.messages
+      chat.channels.find((el) => data.channelID == el.channelID).messages =
+        channel.messages;
 
       var updateObj = {
         channelID: data.channelID,
@@ -111,35 +156,35 @@ io.on("connection", (socket) => {
     }
   });
 
-  // listen for username change 
+  // listen for username change
   socket.on("update_username", (un) => {
     try {
-      stats.connections.find(el => el.id == socket.id).username = un
+      stats.connections.find((el) => el.id == socket.id).username = un;
       // send new user list
-      io.emit("userlist", stats.connections)
+      io.emit("userlist", stats.connections);
     } catch (error) {
       console.log(error);
       io.emit("error", error);
     }
   });
 
-  // listen for color change 
+  // listen for color change
   socket.on("update_color", (col) => {
     try {
-      stats.connections.find(el => el.id == socket.id).color = col
+      stats.connections.find((el) => el.id == socket.id).color = col;
       // send new user list
-      io.emit("userlist", stats.connections)
+      io.emit("userlist", stats.connections);
     } catch (error) {
       console.log(error);
       io.emit("error", error);
     }
   });
-  
+
   // user disconnected
   socket.on("disconnect", () => {
     stats.connections = stats.connections.filter((el) => el.id !== socket.id);
     // send new user list
-    io.emit("userlist", stats.connections)
+    io.emit("userlist", stats.connections);
     console.log(
       stats.connections.length,
       "❌",
